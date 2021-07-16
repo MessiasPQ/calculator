250c32aaa172352c169af2079d2d54602021a1eb
Messias Queiroz <messias1572@hotmail.com>
2021-07-16 08:37:07 -0300

    struct of calculator

--- Program.cs
+++ Program.cs
@@ -6,7 +6,26 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Clear();
+
+            Console.WriteLine("Digite o primeiro valor:");
+            float value1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo valor:");
+            float value2 = float.Parse(Console.ReadLine());
+        }
+
+    }
+
+    struct Calculator
+    {
+        private float Value1;
+        private float Value2;
+
+        public Calculator(float value1, float value2)
+        {
+            Value1 = value1;
+            Value2 = value2;
         }
     }
 }

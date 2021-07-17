811b748e670db09a725fbcf378bb265b02a64126
Messias Queiroz <messias1572@hotmail.com>
2021-07-17 09:20:37 -0300

    Add interaction with the calculator

--- Program.cs
+++ Program.cs
@@ -14,6 +14,29 @@ namespace Calculator
 
             Console.WriteLine("Digite o segundo valor:");
             float value2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Escolha a operação desejada de acordo com sua numeração.");
+            Console.WriteLine("1- Adição / 2- Subtração / 3- Multiplicação / 4-Divisão.");
+            int option = int.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("Valor final: " + calculator.Addition(value1, value2));
+                    break;
+                case 2:
+                    Console.WriteLine("Valor final: " + calculator.Subtraction(value1, value2));
+                    break;
+                case 3:
+                    Console.WriteLine("Valor final: " + calculator.Multiplication(value1, value2));
+                    break;
+                case 4:
+                    Console.WriteLine("Valor final: " + calculator.Division(value1, value2));
+                    break;
+                default:
+                    Console.WriteLine("Valor inválido, tente novamente.");
+                    break;
+            }
         }
 
     }
